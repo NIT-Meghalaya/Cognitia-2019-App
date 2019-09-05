16903ba23de6e64f568b77528f4286f8c65fc2b6
Devansh Maurya <Devansh233@gmail.com>
2019-09-05 16:39:52 +0530

    Initial fragment added

---
+++ app/src/main/java/nitmeghalaya/cognitia2019/main/EventListFragment.kt
@@ -0,0 +1,21 @@
+package nitmeghalaya.cognitia2019.main
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import nitmeghalaya.cognitia2019.R
+
+class EventListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_list, container, false)
+    }
+
+
+}
