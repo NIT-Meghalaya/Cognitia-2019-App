b631d2f11e970e04cfcb26aeb9ea452d07b96b90
Devansh Maurya <Devansh233@gmail.com>
2019-09-19 12:45:23 +0530

    Added splash screen. Started working on navigation drawer.

---
+++ app/src/main/java/nitmeghalaya/cognitia2019/splash/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package nitmeghalaya.cognitia2019.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import nitmeghalaya.cognitia2019.MainActivity
+import nitmeghalaya.cognitia2019.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT = 4000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
