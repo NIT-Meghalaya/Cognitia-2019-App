5907237b318bb7889092d88586d84f8c4fe12def
ichimichi <ichimichilaribok@gmail.com>
2019-09-22 17:37:26 +0530

    fixed team member item incorrectly displayed for HD resolution

--- app/src/main/java/nitmeghalaya/cognitia2019/model/ScheduleItem.kt
+++ app/src/main/java/nitmeghalaya/cognitia2019/model/ScheduleItem.kt
@@ -4,8 +4,5 @@ data class ScheduleItem (
     val name: String = "",
     val startTime: String = "",
     val endTime: String = "",
-    val date: String = "",
-    val image: String = "",
-    var dominantColor: Int = 0,
-    var imageResId: Int = 0
+    var imageLink: String = ""
 )
--- app/src/main/java/nitmeghalaya/cognitia2019/model/TeamMember.kt
+++ app/src/main/java/nitmeghalaya/cognitia2019/model/TeamMember.kt
@@ -3,5 +3,6 @@ package nitmeghalaya.cognitia2019.model
 data class TeamMember(
     val name: String = "",
     val email: String = "",
-    val image: String = ""
+    val image: String = "",
+    val team: String = ""
 )
