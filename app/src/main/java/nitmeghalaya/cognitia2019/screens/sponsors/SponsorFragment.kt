5dc05ec5bfd9b10c0c80501ef4ee3740a319ba28
golupankajgoyal <45711128+golupankajgoyal@users.noreply.github.com>
2019-09-25 16:55:34 +0530

    made changes to contact  fragment

--- app/src/main/java/nitmeghalaya/cognitia2019/screens/sponsors/SponsorFragment.kt
+++ app/src/main/java/nitmeghalaya/cognitia2019/screens/sponsors/SponsorFragment.kt
@@ -8,18 +8,21 @@ import android.view.View
 import android.view.ViewGroup
 
 import nitmeghalaya.cognitia2019.R
+import nitmeghalaya.cognitia2019.screens.BaseFragment
 
 /**
  * A simple [Fragment] subclass.
  */
-class SponsorFragment : Fragment() {
+class SponsorFragment : BaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setActionbarTitle("Sponsors")
         return inflater.inflate(R.layout.fragment_sponsor, container, false)
+
     }
 
 
