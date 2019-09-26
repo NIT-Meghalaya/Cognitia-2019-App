929297ab2666c5c7af5ae359f247f3b1150abfe1
ichimichi <ichimichilaribok@gmail.com>
2019-09-26 13:10:38 +0530

    updated intro slider to show on first start

--- app/src/main/java/nitmeghalaya/cognitia2019/screens/splash/SplashScreenActivity.kt
+++ app/src/main/java/nitmeghalaya/cognitia2019/screens/splash/SplashScreenActivity.kt
@@ -3,8 +3,10 @@ package nitmeghalaya.cognitia2019.screens.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
 import nitmeghalaya.cognitia2019.R
+import nitmeghalaya.cognitia2019.screens.MainActivity
 import nitmeghalaya.cognitia2019.screens.introslider.IntroSliderActivity
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -15,8 +17,18 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        val getPrefs = PreferenceManager
+            .getDefaultSharedPreferences(baseContext)
+
+        val isFirstStart = getPrefs.getBoolean("firstStart", true)
+
         Handler().postDelayed({
-            startActivity(Intent(this, IntroSliderActivity::class.java))
+            if (isFirstStart) {
+                startActivity(Intent(this, IntroSliderActivity::class.java))
+            } else {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+
             finish()
         }, SPLASH_TIME_OUT)
     }
